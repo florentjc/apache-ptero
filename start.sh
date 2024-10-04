8e0823cf57e5936a206383a1fc0f0c2524d0e245
Florent <52673722+florentjc@users.noreply.github.com>
2024-10-04 22:55:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,8 @@
 rm -rf /home/container/tmp/*
 PHP_VERSION=$(cat "/home/container/php_version.txt")
 
-# Démarrer PHP-FPM
 echo "⟳ Starting PHP-FPM..."
 php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
 
-# Démarrer Apache
 echo "⟳ Starting Apache..."
 echo "✓ Services successfully launched"
-apachectl -D FOREGROUND
