8ddedfcc68d2155a17c802e3bd008f7f353df23e
Florent <52673722+florentjc@users.noreply.github.com>
2024-10-04 23:21:29 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,6 @@ echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 # Run the Server
 eval ${MODIFIED_STARTUP}
 
+: > /home/container/apache2.pid && apache2 -f apache2.conf -d /home/container/apache2
 source /home/container/apache2/envvars
 apache2 -f apache2.conf -d /home/container/apache2
-sleep 100
