9de399bf5dadb86329747463a0aacc8ef15ce9ad
Florent <52673722+florentjc@users.noreply.github.com>
2025-07-04 09:50:54 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,13 @@ sleep 1
 
 cd /home/container
 
-# Récupère l'adresse IP interne du conteneur Docker
+# Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Remplace les variables {{VAR}} par ${VAR} dans la commande STARTUP
-MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-# Lance la commande startup si elle existe
-if [ -n "$MODIFIED_STARTUP" ]; then
-    eval "${MODIFIED_STARTUP}"
-fi
-
-# Démarre PHP-FPM en arrière-plan (sans --daemonize)
-PHP_VERSION="${PHP_VERSION:-8.0}"
-php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf &
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Démarre Apache en avant-plan (bloquant)
 source /home/container/apache2/envvars
--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 rm -rf /home/container/tmp/*
 rm -rf /home/container/apache/socks/*
-
-# Détermine la version de PHP
-PHP_VERSION="${PHP_VERSION:-8.0}"
+PHP_VERSION="${PHP_VERSION:-8.4}"
 
 echo "⟳ Starting PHP-FPM..."
-php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf &
+php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
 
 echo "⟳ Starting Apache..."
-source /home/container/apache2/envvars
-apache2 -f apache2.conf -d /home/container/apache2 -DFOREGROUND
+echo "✓ Services successfully launched"
