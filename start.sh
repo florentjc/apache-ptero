4fddbbfd185aba1cedfe91f1e6edc12adaa37cf1
Florent <52673722+florentjc@users.noreply.github.com>
2025-07-04 08:54:03 +0200

    Updated

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf /home/container/tmp/*
 rm -rf /home/container/apache/socks/*
-PHP_VERSION=$(cat "/home/container/php_version.txt")
+PHP_VERSION="${PHP_VERSION:-8.4}"
 
 echo "⟳ Starting PHP-FPM..."
 php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
